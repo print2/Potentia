41cc4d33e624f40157aa75618c17a52a8fc1cdaa
Seb <sebsmith2002@gmail.com>
2022-01-29 20:46:22 +0000

    Added functionality to automatically detect new 'connectable' smart plugs

--- connection.py
+++ connection.py
@@ -11,51 +11,6 @@ from pymongo import MongoClient
 cluster=MongoClient("mongodb+srv://230GRP4:HklMriJ6iK8iU8n5@cluster0.wl3na.mongodb.net/Plugs?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 db=cluster["Plugs"]
 collection=db["UsageData"]
-##   IMPORTANT INFO   ##
-
-    #Using python-kasa instead of pyHS100
-        #install using pip install python-kasa
-        #this provides the wifi_join smart plug method
-
-    #uses a linux network manager NMCLI
-        #allows use of command line commands to scan for and connect to networks
-        #install using
-            #apt-get update
-            #apt-get upgrade
-            #apt-get install network-manager
-
-            #IF FAILS
-                #try "sudo apt-get install network-manager"
-                #sudo runs command with all privileges by default
-
-    #uses asyncio
-        #only using it because python-kasa does so cant run any of its methods without
-        #install using pip install asyncio
-
-    #I think those three are the only extra things that need installing, let me know if im wrong
-
-
-
-##   HOW IT WORKS   ##
-
-    #Scans available networks
-    #Finds current network
-    #Finds smart plug network
-    #Connects to smart plug network
-    #Scans for devices on smart plug network
-        #Should be only smart plug
-    #Get plug IP and MAC
-    #Create plug object and use wifi_join to connect plug to home network
-    #Connect pi back to home network
-    #Find new IP of plug by scanning for known MAC
-    #create new plug object, with plug and pi both on home network
-    #Can then manipulate the plug however you like
-
-
-##   Things to note   ##
-
-    #SP still detects power for about 30 seconds after turning off the plug via the program
-
 
 def getNetworksInfo(attempts=3,attemptDelay=5):
     cListNets = "nmcli dev wifi list" #lists available networks
@@ -125,28 +80,18 @@ def findSpSSIDs():
 
     return spSSIDs,currNet,spFound
 
-def getPlugsOnNet():
-    plugsFound = asyncio.run(Discover.discover())
-    return plugsFound.values()
+async def getPlugsOnNet():
+    plugsFound = await Discover.discover()
+    return plugsFound
 
-def connPlugToHome(plugs,ssid,password):
+async def connPlugToHome(plugs,ssid,password):
     for plug in plugs:
-        asyncio.run(actOnPlugs(plug,ssid,password))
+        await actOnPlugs(plug,ssid,password)
 
 async def actOnPlugs(plug,ssid,password):
     alias = input("Give this plug a name: ")
     await plug.set_alias(alias)
     await plug.wifi_join(ssid,password)
-
-#reads power every second
-async def readPower(plugs):
-    plugTasks = []
-    for plug in plugs:
-        plugTasks.append(asyncio.create_task(readSingle(plug)))
-    
-    for task in plugTasks:
-        await task
-
 
 async def readSingle(plug):
     while(True):
@@ -156,56 +101,61 @@ async def readSingle(plug):
             
             print(plug.alias + " is currently using: " + str(power) + " W")
         except:
-            print("ERROR: " + plug.alias + " not found")
+            pass
 
         post={"name": plug.alias,"Power": power, "date/time": datetime.now()}
         collection.insert_one(post)
 
         await asyncio.sleep(1)
 
+async def detectNewPlug(prevPlugs):
+    newPlugs = await getPlugsOnNet()
+    for ip in newPlugs:
+        if ip not in prevPlugs:
+            return newPlugs[ip]
 
-def connectPlug(plugSSID,homeNet,homePass):
+async def connectPlug(plugSSID,homeNet,homePass,currPlugs):
     if(connectTo(plugSSID)):
-        connPlugToHome(getPlugsOnNet(),homeNet,homePass)
+        plugsOnNet = await getPlugsOnNet()
+        await connPlugToHome(plugsOnNet.values(),homeNet,homePass)
         connectTo(homeNet,homePass)
-            
-def main():
-    plugSSIDs,homeNet,found = findSpSSIDs()
+        return await detectNewPlug(currPlugs)
+
+async def connToAll(homePass):
+    plugsToConnect,homeNet,found = findSpSSIDs()
+    print("checking for new smart plugs")
+
     if(found):
-        homePass = input("What is the password to " + homeNet + "?")
-        for x in plugSSIDs:
-            connectPlug(x,homeNet,homePass)
+        print("found new smart plug")
+        for ssid in plugsToConnect:
+            plugsOnNet = await getPlugsOnNet()
+            plug = await connectPlug(ssid,homeNet,homePass,plugsOnNet)
+            asyncio.ensure_future(readSingle(plug),loop=event_loop)
 
-    discoveredPlugs = asyncio.run(Discover.discover())
-    connectedPlugs = discoveredPlugs.values()
-    for plug in connectedPlugs:
-        print(plug.alias)
+async def scanForPlugs(homePass):
+    connectedPlugs = await getPlugsOnNet()
+    for ip in connectedPlugs:
+        asyncio.ensure_future(readSingle(connectedPlugs[ip]),loop=event_loop)
 
-    asyncio.run(readPower(connectedPlugs))
-            
-       
-    return homeNet,homePass
+    while True:
+        await connToAll(homePass)
+        await asyncio.sleep(60)
+
+def main():
+    global event_loop
+    event_loop = asyncio.get_event_loop()
+
+    _ssids,homeNet,_found = findSpSSIDs()
+
+    homePass = input("What is the password to " + homeNet + "?")
+
+    event_loop.run_until_complete(scanForPlugs(homePass))
+    event_loop.close()
 
 if __name__ == "__main__":
-    mainNet,mainPass = main()
+    main()
     print("Program terminated")
 
-    #supposedly connects back to home network if program fails when on SP network but havent tested this
-    _ssids,_passed,currNet = getSSIDs()
-    if(currNet != mainNet):
-        connectTo(mainNet,mainPass)
 
 
-
-#when connect to plug, pi joins smart plug network, then connects to original network
-
-#have to wait for one to connect, before doing anything
-
-#2 connected and giving data
-#try to connect another one, must stop the other two from transferring data temporarily
-
-#handle when plug disconnects  
-    #try and except, until we rediscover devices
-
-#Dont ask for wifi password if no devices to connect
-#COnnect device to device
+#delay when plug connects before it reads - seems to pause all threads*
