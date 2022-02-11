c73a10b0a537ebd4949fbc4dfab1bfc95dc935a4
Seb <sebsmith2002@gmail.com>
2022-02-11 15:59:34 +0000

    Added a universal string to list parser to FlaskExecutor. Declared appliances in appDriver class

--- mobileApp/main/java/app/potentia/classes/FlaskExecutor.java
+++ mobileApp/main/java/app/potentia/classes/FlaskExecutor.java
@@ -7,39 +7,53 @@ import java.net.URL;
 public class FlaskExecutor {
 
     protected String execFlaskMethod(String methodName, ArrayList<String> parameters){
-            String ip = "192.168.43.24:5000";
-            String accessUrl = "http://" + ip +"/" + methodName + "/";
-            for (String parameter : parameters){
-                accessUrl = accessUrl + parameter + "&";
+        String ip = "192.168.43.24:5000";
+        String accessUrl = "http://" + ip +"/" + methodName + "/";
+        for (String parameter : parameters){
+            accessUrl = accessUrl + parameter + "&";
+        }
+        if(parameters.size() > 0){
+            accessUrl = accessUrl.substring(0,accessUrl.length() -1);
+        }
+
+        try{
+            URL url = new URL(accessUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if(conn.getResponseCode() != 200){
+                throw new RuntimeException ("Failed : HTTP error code: " + conn.getResponseCode());
             }
-            if(parameters.size() > 0){
-                accessUrl = accessUrl.substring(0,accessUrl.length() -1);
-            }
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            
+            String output = br.readLine();
+            System.out.println("Output from server ... \n");
 
-            try{
-                URL url = new URL(accessUrl);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Accept", "application/json");
-                if(conn.getResponseCode() != 200){
-                    throw new RuntimeException ("Failed : HTTP error code: " + conn.getResponseCode());
-                }
-                
-                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                
-                String output = br.readLine();
-                System.out.println("Output from server ... \n");
+            conn.disconnect();
 
-                conn.disconnect();
+            return output;
 
-                return output;
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+            return "failed MURL";
+        } catch(IOException e){
+            e.printStackTrace();
+            return "failed IO";
+        }
+    }
 
-            } catch (MalformedURLException e){
-                e.printStackTrace();
-                return "failed MURL";
-            } catch(IOException e){
-                e.printStackTrace();
-                return "failed IO";
-            }
+    protected ArrayList<String> stringToList(String string){
+        ArrayList<String> list = new ArrayList<>();
+
+        int cutoff = string.indexOf('|');
+        while (cutoff != -1){
+            list.add(string.substring(1,cutoff));
+             string = string.substring(cutoff+1,string.length() -1);
+            cutoff = string.indexOf('|');
+        }
+        list.add(string);
+
+        return list;
     }
 }
--- mobileApp/main/java/app/potentia/classes/testDriver.java
+++ mobileApp/main/java/app/potentia/classes/testDriver.java
@@ -1,39 +1,8 @@
 import java.util.*;
 
 public class testDriver {
-    private static applianceProfile fridge = new applianceProfile("Fridge", true);
-    private static applianceProfile kettle = new applianceProfile("Kettle",false,10,20);
-    private static applianceProfile charger = new applianceProfile("Charger",false,180,0);
-    private static applianceProfile microwave = new applianceProfile("Microwave",false,60,120);
-    private static applianceProfile television = new applianceProfile("Television",false,120,60);
-    private static applianceProfile lamp = new applianceProfile("Lamp",false,120,60);
-    private static applianceProfile dishwasher = new applianceProfile("Dishwasher",false,180,120);
-    private static applianceProfile electricBlanket = new applianceProfile("Electric Blanket",false,180,60);
-    private static applianceProfile electricHeater = new applianceProfile("Electric Heater",false,120,60);
-    private static applianceProfile freezer = new applianceProfile("Freezer",true);
-    private static applianceProfile oven = new applianceProfile("Oven",false,240,120);
-    private static applianceProfile toaster = new applianceProfile("Toaster",false,10,10);
-    private static applianceProfile washingMachine = new applianceProfile("Washing Machine",false,240,120);
-
-
-
-    private static ArrayList<applianceProfile> applianceList = new ArrayList<>();
 
     public static void main(String[] args){
-        applianceList.add(fridge);
-        applianceList.add(kettle);
-        applianceList.add(charger);
-        applianceList.add(microwave);
-        applianceList.add(television);
-        applianceList.add(lamp);
-        applianceList.add(dishwasher);
-        applianceList.add(electricBlanket);
-        applianceList.add(electricHeater);
-        applianceList.add(freezer);
-        applianceList.add(oven);
-        applianceList.add(toaster);
-        applianceList.add(washingMachine);
-
         plugProfile plug = new plugProfile("Test");
         // plug.setIP("192.168.43.28");
         // plug.retrieveCurrUsage();
