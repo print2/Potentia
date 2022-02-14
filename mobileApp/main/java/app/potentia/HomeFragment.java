9c9a487d05c10f5bbed247758b9b89817bda1c6a
Nuria <n.torresramon@lancaster.ac.uk>
2022-02-14 02:15:56 +0000

    Created graph

--- mobileApp/main/java/app/potentia/HomeFragment.java
+++ mobileApp/main/java/app/potentia/HomeFragment.java
@@ -1,33 +1,40 @@
 package app.potentia;
 
-import static java.lang.Thread.sleep;
-
+import android.os.Handler;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class HomeFragment extends Fragment implements View.OnClickListener{
+public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+
+    //need function to return plug object from given name
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
     private View inflatedView;
-    private Button button;
+//    private Button button;
+    private Spinner dropdown;
     private TextView currentUsage;
     private String string;
 
-    public plugProfile plug = new plugProfile("name");
+    private appDriver appDriver = new appDriver();
+    private ArrayList<String> connected = new ArrayList<String>();
+
+    public plugProfile currentPlug;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,28 +42,55 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
         this.inflatedView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        button = inflatedView.findViewById(R.id.button);
-        button.setOnClickListener(this);
+//        button = inflatedView.findViewById(R.id.button);
+//        button.setOnClickListener(this);
+        connected.add("Smart Plug 1");
 
+        //connected = appDriver.getConnectedPlugs();
         currentUsage = inflatedView.findViewById(R.id.currentUsage);
-        currentUsage.setOnClickListener(this);
+        dropdown = inflatedView.findViewById(R.id.dropdown);
+        currentUsage.setText("*** kWh");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflatedView.getContext(), android.R.layout.simple_spinner_item, connected) ; //?
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
 
         return inflatedView;
     }
 
+//    @Override
+//    public void onClick(View view) {
+//        currentPlug.getIP();
+//        currUsageAsyncTask(currentPlug);
+//    }
     @Override
-    public void onClick(View view) {
-        plug.setIP("192.168.43.28");
-        currentUsage.setVisibility(currentUsage.VISIBLE);
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
-        currUsageAsyncTask();
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
     }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            //currUsageAsyncTask(currentPlug);
+        }
 
     public class Async extends AsyncTask<plugProfile, Void, String>{
 
         @Override
         protected String doInBackground(plugProfile... params) {
-            string = plug.retrieveCurrUsage();
+            string = currentPlug.retrieveCurrUsage();
             return string;
         }
 
@@ -67,7 +101,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
     }
 
-    private void currUsageAsyncTask() {
+    private void currUsageAsyncTask(plugProfile plug) {
 
         final Handler handler = new Handler();
         Timer timer = new Timer();
